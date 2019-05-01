72910219e9462002eb29287dff8d6e94113f2bc1
em <emilygarcia1123@gmail.com>
2019-05-01 08:52:16 -0700

    Added script for fireball damage

---
+++ Assets/Scripts/DoDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoDamage : MonoBehaviour
+{
+    public float damage;
+    public Fighter owner;
+
+    void Start()
+    {
+        owner = gameObject.GetComponentInParent<Fighter>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Fighter somebody = other.gameObject.GetComponent<Fighter>();
+        if (owner.Attacking)
+        {
+            if (somebody != null && somebody != owner)
+            {
+                somebody.Hurt(damage);
+            }
+        }
+    }
+}
